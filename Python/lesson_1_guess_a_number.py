917558a6b3f2e1c0d4026672c0031b0bd6523d30
Дорохин Святослав <svyatoslav.dorokhin@playrix.com>
2025-02-16 16:29:55 +0500

    Урок 1 Дополнил игру
    Добавил номер попытки в каждую строку. Добавил возможность перезапуска игры

--- Python/lesson_1_guess_a_number.py
+++ Python/lesson_1_guess_a_number.py
@@ -1,30 +1,39 @@
 from random import randint
 
-random_number = randint(1, 100)
-attempt_counter = 0
-user_input = int(input("Let's play the game! I'll guess the number, and you try to guess it, take a number from 1 to 100: "))
+def choose_game():
+    attempt_counter = 0
+    max_range = int(input("Let's play the game! I'll guess the number, and you try to guess it. Choose, how big our range is: "))
+    user_input = int(input(f"Now, take a number from 1 to {max_range}: "))
+    random_number = randint(1, max_range)
+    while True:
+        if 1 > user_input or user_input > max_range:
+            user_input = int(input(f"invalid number, please enter a number from 1 to {max_range}: "))
+            continue
+            
+        attempt_counter += 1
+        if user_input > random_number:
+            if  user_input - random_number == 1:
+                user_input = int(input(f"#{attempt_counter} So close, enter a number: "))
+            elif user_input - random_number < 10:
+                user_input = int(input(f"#{attempt_counter} Very close, you need a smaller number, enter a number: "))
+            else:
+                user_input = int(input(f"#{attempt_counter} Oh, that's too much, let's enter a number: "))
+        elif user_input < random_number:
+            if  random_number - user_input == 1:
+                user_input = int(input(f"#{attempt_counter} So close, enter a number: "))
+            elif  random_number - user_input < 10:
+                user_input = int(input(f"#{attempt_counter} Very close, you need a higher number, enter a number: "))
+            else:
+                user_input = int(input(f"#{attempt_counter} Oh, that's too little, let's enter a number: "))
+        else:
+            print(f"You guessed right! The count of your attempts is: {attempt_counter}.")
+            return
 
 while True:
-    attempt_counter += 1
-    if 1 > user_input > 100:
-        user_input = int(input("invalid number, please try again: "))
-    elif user_input > random_number:
-        if  user_input - random_number == 1:
-            user_input = int(input("so close, try again: "))
-        elif user_input - random_number < 10:
-            user_input = int(input("very close, you need a smaller number, try again: "))
-        else:
-            user_input = int(input("Oh, that's too much, let's try again: "))
-    elif user_input < random_number:
-        if  random_number - user_input == 1:
-            user_input = int(input("so close, try again: "))
-        elif  random_number - user_input < 10:
-            user_input = int(input("very close, you need a higher number, try again: "))
-        else:
-            user_input = int(input("Oh, that's too little, let's try again: "))
-    else:
-        print(f"You guessed right! The count of your attempts is: {attempt_counter}.")
+    choose_game()
+    if input('Do you want to repeat? (y/n) ').lower() != 'y':
         break
+print('Game over')
 
 
 
